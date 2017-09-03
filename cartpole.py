d3117fe1b272db9d66bd6177c905a5926ce68909
sahasd <sahasd@gmail.com>
2017-09-03 09:46:51 -0700

    failed attempt

--- cartpole.py
+++ cartpole.py
@@ -3,4 +3,5 @@ env = gym.make('CartPole-v0')
 env.reset()
 for _ in range(1000):
     env.render()
-    env.step(env.action_space.sample()) # take a random action
+    next_state, reward, done,_ = env.step(env.action_space.sample())
+    print(next_state, reward, done,_)
